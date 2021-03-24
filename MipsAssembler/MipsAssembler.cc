88b2c81ca42fade3d1d2f15f2cf0c4a11fd1752e
chlorUpload <chlor.upload@gmail.com>
2021-03-25 01:10:33 +0900

    변경 사항 적용 테스트 코드 작성

--- MipsAssembler/MipsAssembler.cc
+++ MipsAssembler/MipsAssembler.cc
@@ -48,41 +48,42 @@ std::vector<std::pair<TokenType, ElementType>> elemDefs = {
     { TokenType::BRAKET_RIGHT, ElementType::TOKEN_BRAKET_RIGHT },
     { TokenType::COMMA, ElementType::TOKEN_COMMA },
 };
-std::vector<std::tuple<TokenType, InstructionId, InstructionFormat>> instDefs = {
-    { TokenType::INST_ADDIU, InstructionId::INST_ADDIU, InstructionFormat::I_FORMAT },
-    { TokenType::INST_ADDU, InstructionId::INST_ADDU, InstructionFormat::R_FORMAT },
-    { TokenType::INST_AND, InstructionId::INST_AND, InstructionFormat::R_FORMAT },
-    { TokenType::INST_ANDI, InstructionId::INST_ANDI, InstructionFormat::I_FORMAT },
-    { TokenType::INST_BEQ, InstructionId::INST_BEQ, InstructionFormat::I_FORMAT },
-    { TokenType::INST_BNE, InstructionId::INST_BNE, InstructionFormat::I_FORMAT },
-    { TokenType::INST_J, InstructionId::INST_J, InstructionFormat::J_FORMAT },
-    { TokenType::INST_JAL, InstructionId::INST_JAL, InstructionFormat::J_FORMAT },
-    { TokenType::INST_JR, InstructionId::INST_JR, InstructionFormat::R_FORMAT },
-    { TokenType::INST_LUI, InstructionId::INST_LUI, InstructionFormat::I_FORMAT },
-    { TokenType::INST_LW, InstructionId::INST_LW, InstructionFormat::I_FORMAT },
-    { TokenType::INST_LB, InstructionId::INST_LB, InstructionFormat::I_FORMAT },
-    { TokenType::INST_NOR, InstructionId::INST_NOR, InstructionFormat::R_FORMAT },
-    { TokenType::INST_OR, InstructionId::INST_OR, InstructionFormat::R_FORMAT },
-    { TokenType::INST_ORI, InstructionId::INST_ORI, InstructionFormat::I_FORMAT },
-    { TokenType::INST_SLTIU, InstructionId::INST_SLTIU, InstructionFormat::I_FORMAT },
-    { TokenType::INST_SLTU, InstructionId::INST_SLTU, InstructionFormat::R_FORMAT },
-    { TokenType::INST_SLL, InstructionId::INST_SLL, InstructionFormat::R_FORMAT },
-    { TokenType::INST_SRL, InstructionId::INST_SRL, InstructionFormat::R_FORMAT },
-    { TokenType::INST_SW, InstructionId::INST_SW, InstructionFormat::I_FORMAT },
-    { TokenType::INST_SB, InstructionId::INST_SB, InstructionFormat::R_FORMAT },
-    { TokenType::INST_SUBU, InstructionId::INST_SUBU, InstructionFormat::R_FORMAT },
+std::vector<std::tuple<TokenType, InstructionId, InstructionType>> instDefs = {
+    { TokenType::INST_ADDIU, InstructionId::INST_ADDIU, InstructionType::I_FORMAT },
+    { TokenType::INST_ADDU, InstructionId::INST_ADDU, InstructionType::R_FORMAT },
+    { TokenType::INST_AND, InstructionId::INST_AND, InstructionType::R_FORMAT },
+    { TokenType::INST_ANDI, InstructionId::INST_ANDI, InstructionType::I_FORMAT },
+    { TokenType::INST_BEQ, InstructionId::INST_BEQ, InstructionType::I_FORMAT_BRANCH },
+    { TokenType::INST_BNE, InstructionId::INST_BNE, InstructionType::I_FORMAT_BRANCH },
+    { TokenType::INST_J, InstructionId::INST_J, InstructionType::J_FORMAT },
+    { TokenType::INST_JAL, InstructionId::INST_JAL, InstructionType::J_FORMAT },
+    { TokenType::INST_JR, InstructionId::INST_JR, InstructionType::R_FORMAT_JR },
+    { TokenType::INST_LUI, InstructionId::INST_LUI, InstructionType::I_FORMAT_LUI },
+    { TokenType::INST_LW, InstructionId::INST_LW, InstructionType::I_FORMAT_OFFSET },
+    { TokenType::INST_LB, InstructionId::INST_LB, InstructionType::I_FORMAT_OFFSET },
+    { TokenType::INST_NOR, InstructionId::INST_NOR, InstructionType::R_FORMAT },
+    { TokenType::INST_OR, InstructionId::INST_OR, InstructionType::R_FORMAT },
+    { TokenType::INST_ORI, InstructionId::INST_ORI, InstructionType::I_FORMAT },
+    { TokenType::INST_SLTIU, InstructionId::INST_SLTIU, InstructionType::I_FORMAT },
+    { TokenType::INST_SLTU, InstructionId::INST_SLTU, InstructionType::R_FORMAT },
+    { TokenType::INST_SLL, InstructionId::INST_SLL, InstructionType::R_FORMAT_SHAMT },
+    { TokenType::INST_SRL, InstructionId::INST_SRL, InstructionType::R_FORMAT_SHAMT },
+    { TokenType::INST_SW, InstructionId::INST_SW, InstructionType::I_FORMAT_OFFSET },
+    { TokenType::INST_SB, InstructionId::INST_SB, InstructionType::I_FORMAT_OFFSET },
+    { TokenType::INST_SUBU, InstructionId::INST_SUBU, InstructionType::R_FORMAT },
 };
 
 int main()
 {
     std::string str = R"===(
-       addu    $10,$9,  zero 
+       addu    $10,$9,  $0 
        or    $4,  $3,  $2  
        srl    $11,$6,  5  
        lw $9, 0($8)
        addu $2, $0, $9
        jal sum
        j exit
+empty_inst_label:
 sum:    sltiu $1, $2, 1
         bne $1, $0, sum_exit
         addu $3, $3, $2
@@ -92,7 +93,9 @@ sum_exit:
         addu $4, $3, $0
         jr $31
 exit:
-        addu $10, $9, zero       
+        addu $10, $9, $0      
+empty_inst_label2:
+
 )===";
 
 #pragma region Tokenizer
@@ -132,7 +135,7 @@ exit:
         parser.AddElementDefinition(
             ElementType::INSTRUCTION,
             ElementDefinitionFactory::CreateInstructionDefinition(
-                std::get<InstructionId>(instDef), std::get<InstructionFormat>(instDef)));
+                std::get<InstructionId>(instDef), std::get<InstructionType>(instDef)));
     }
 
     parser.AddElementDefinition(ElementType::OFFSET_ADDRESS,
@@ -144,8 +147,8 @@ exit:
 
     ReadableSerializer serializer;
 
-    for (auto iter = tokenizer.begin(); iter != iter.end(); iter++)
-    { std::cout << "Type : " << (int)iter->type << "\tValue : " << iter->value << std::endl; }
+    // for (auto iter = tokenizer.begin(); iter != iter.end(); iter++)
+    //{ std::cout << "Type : " << (int)iter->type << "\tValue : " << iter->value << std::endl; }
 
     for (auto iter = tokenizer.begin(); iter != iter.end();)
     {
